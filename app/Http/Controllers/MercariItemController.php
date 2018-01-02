291160d4f1eeaf2e1d2ca925650b2f476dc70367
Hiryu Kawaguchi <hiryu.tubasa@gmail.com>
2018-01-02 15:06:49 +0900

    test

--- app/Http/Controllers/MercariItemController.php
+++ app/Http/Controllers/MercariItemController.php
@@ -17,12 +17,17 @@ class MercariItemController extends Controller
         $keyword->keyword = str_replace(" ", "+", $keyword->keyword);
         $api_id = $keyword->id;
         $url = "https://www.mercari.com/jp/search/?sort_order=&keyword=".$keyword->keyword."&category_root=&brand_name=&brand_id=&size_group=&price_min=".$keyword->price_min."&price_max=".$keyword->price_max;
+        $proxy = 'http://160.202.42.106:8080';
         $client2 = new Client();
         //$client2->setHeader('User-Agent', 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)');
         //$client2->setHeader('accept-language', 'ja');
         //$client2->setHeader('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8');
         $client2->setHeader('accept-encoding', 'gzip, deflate, br');
-        $crawler = $client2->request('GET', $url);
+        $crawler = $client2->request('GET', $url,['proxy' => $proxy]);
+        //
+        var_dump($crawler);
+        die();
+
         $items = $crawler->filter('div.items-box-content')->filter('section.items-box')->each(function($element) use (&$i,&$List,&$api_id){
             $List[$i]['name'] = $element->filter('h3')->text();
             $List[$i]['price'] = $element->filter('div.items-box-price')->text();
